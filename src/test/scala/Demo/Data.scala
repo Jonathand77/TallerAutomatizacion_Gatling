3a8615a9dda65d789482e9ae69f13c4e4ef3aa6c
Jonathan Fernandez <138726219+Jonathand77@users.noreply.github.com>
2025-06-26 19:53:19 -0500

    Update Data.scala

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -2,6 +2,6 @@ package Demo
 
 object Data{
     val url= "https://thinking-tester-contact-list.herokuapp.com/"
-    val email = "jonathand.fernandez@udea.edu.co"
+    val email = "jonathanGatlin@test.com"
     val password = "12345678"
 }
